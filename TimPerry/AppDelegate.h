43a17b44d32b5c4714c0608ab04c1bee63c31edf
Tim Perry <tim.r.perry@gmail.com>
2015-07-21 19:28:52 +0100

    Initial Commit

---
+++ TimPerry/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TimPerry
+//
+//  Created by Tim Perry on 21/07/2015.
+//  Copyright (c) 2015 Tim Perry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TimPerry/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TimPerry
+//
+//  Created by Tim Perry on 21/07/2015.
+//  Copyright (c) 2015 Tim Perry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
